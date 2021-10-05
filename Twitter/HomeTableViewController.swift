eb2e0a9c585917fd6c7dc9e474c115e8d6779284
philipjpark <philipparknyc@gmail.com>
2021-10-05 17:05:30 -0400

    User can retweet a tweet

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -103,7 +103,7 @@ let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
         
         cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
         cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
-        
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         return cell
     }
     
--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -36,10 +36,26 @@ class TweetCellTableViewCell: UITableViewCell {
     }
     
     @IBAction func retweet(_ sender: Any) {
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+            self.setRetweeted(true)
+        }, failure: { (error) in
+            print("Error is retweeting: \(error)")
+        })
+    }
+    
+    func setRetweeted(_ isRetweeted:Bool){
+        if(isRetweeted) {
+            retweetButton.setImage(UIImage(named: "retweet-icon-green"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+        } else {
+            retweetButton.setImage(UIImage(named: "retweet-icon"), for: UIControl.State.normal)
+            retweetButton.isEnabled = true
+        }
     }
     
     var favorited:Bool = false
-    var tweetId:Int = -1 
+    var tweetId:Int = -1
+    //var retweeted: Bool = false
     
     func setFavorite(_ isFavorited: Bool) {
         favorited = isFavorited
@@ -50,6 +66,7 @@ class TweetCellTableViewCell: UITableViewCell {
             favButton.setImage(UIImage(named: "favor-icon"), for: UIControl.State.normal)
         }
     }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
